a93a48525148587a7ae66d35f2063d01422844b0
정준호 <yks3858@gmail.com>
2020-09-15 17:27:22 +0900

    rewqrwqer

---
+++ JJH_2020.09.15start/Class1.cs
@@ -0,0 +1,109 @@
+﻿using JJH_2020_09_15start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJH_2020_09_15start
+{
+    class AccountManager
+    {
+        BankAccount test1;
+        
+        
+        
+        int money;
+
+        public void PrintMenu()     //계좌관리프로그램 메뉴
+        {
+            Console.WriteLine("\n---Menu--------");
+            Console.WriteLine("1. 계좌 개설");
+            Console.WriteLine("2. 입금");
+            Console.WriteLine("3. 출금");
+            Console.WriteLine("4. 잔액 조회");
+            Console.WriteLine("5. 프로그램 종료");
+            Console.WriteLine();
+        }
+
+        public void MakeAccount()   //계좌 개설
+        {
+            int test = 10001;
+
+            Console.Write("예금주를 입력해주세요 : ");
+            string name = Console.ReadLine();
+            string AccNum = $"1111-111-{test}";
+            Console.WriteLine($"{name}님의 계좌번호는 1111-111-{test} 입니다");
+
+            test1 = new BankAccount(AccNum,name);
+            Console.WriteLine("개설이 완료되었습니다");
+            Console.WriteLine();
+            test++;
+        }
+
+        public void Deposit()       //입금
+        {
+            Console.Write("입금하실 금액은? :");
+            int money = int.Parse(Console.ReadLine());
+            test1.InputMoney(money);
+            Console.WriteLine($"{money}원 입금 되었습니다");
+            Console.WriteLine();
+        }
+
+        public void Withdrawal()    //출금
+        {
+            Console.Write("출금하실 금액은? : ");
+            int money = int.Parse(Console.ReadLine());
+            test1.OutputMoney(money);
+            Console.WriteLine($"{money}원 출금 되었습니다");
+            Console.WriteLine();
+        }
+
+        public void BalanceCheck()  //잔액 조회
+        {
+            int balance = test1.Balance;
+            Console.WriteLine($"{test1.Name}님의 잔액은 {balance}원 입니다");
+            Console.WriteLine();
+        }
+    }
+
+    class AccountProgram
+    {
+
+        static void Main()
+        {
+
+            AccountManager manager = new AccountManager();
+            Console.WriteLine(manager.ToString());
+
+            int choice;
+
+            while (true)
+            {
+                manager.PrintMenu();
+                Console.Write("선택 : ");
+                choice = int.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        manager.MakeAccount(); break;
+                    case 2:
+                        manager.Deposit(); break;
+                    case 3:
+                        manager.Withdrawal(); break;
+                    case 4:
+                        manager.BalanceCheck(); break;
+                    case 5: return;
+                    default:
+                        Console.WriteLine("다시 선택하세요"); break;
+
+                }
+
+            }
+
+
+            
+        }
+    }
+}
---
+++ JJH_2020.09.15start/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJH_2020_09_15start
+{
+
+    class BankAccount
+    {
+        private static double interest = 0.2;   //이자율
+        private string accNum;           //계좌번호
+        private string name;             //예금주명
+        private int balance;             //잔액        
+
+        #region property
+        public string AccNum
+        {
+            get { return accNum; }
+            set { accNum = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Balance //읽기전용속성
+        {
+            get { return balance; }
+        }
+        #endregion
+
+        #region ctor
+        public BankAccount()
+        {
+            balance = 10;
+            interest = 0.2;
+        }
+
+        public BankAccount(string accNum, string accName)
+        {
+            this.accNum = accNum;
+            Name = accName;
+            balance = 10;
+        }
+        #endregion
+
+        #region method
+        //출금
+        public string OutputMoney(int amount)
+        {
+            if (balance < amount)
+            {
+                string msg = "잔액이 부족합니다";
+                return msg;
+            }
+
+            balance = balance - amount;
+            return "";
+        }
+        /*
+        public void OutputMoney(int amount)
+        {
+            if (balance < amount)
+            {
+                Console.WriteLine("잔액이 부족합니다");
+                return;
+            }
+
+            balance = balance - amount;
+        }
+        */
+
+        //예금
+        public void InputMoney(int amount)
+        {
+            balance = balance + amount + (int)(amount * interest);
+        }
+
+        //계좌정보 출력
+        public void PrintAccInfo()
+        {
+            Console.WriteLine($"계좌번호:{this.accNum} / 예금주명:{this.Name} / 잔액:{this.balance} / 이자율:{BankAccount.interest}%");
+        }
+
+        //이자율 변경
+        public void SetInterest(double interest)
+        {
+            BankAccount.interest = interest;
+            balance += 10;
+        }
+        #endregion
+    }
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        BankAccount acc0 = new BankAccount();
+    //        acc0.AccNum = "111-11111";
+    //        acc0.Name = "홍길동";
+    //        acc0.PrintAccInfo();
+    //        acc0.SetInterest(10);
+
+
+
+    //        BankAccount acc1 = new BankAccount("222-22222", "류현진");
+    //        acc1.Name = "손흥민";
+    //        acc1.PrintAccInfo();
+
+    //        acc0.InputMoney(500);
+
+    //        string errMsg = acc0.OutputMoney(100);
+    //        if (errMsg.Length > 0)
+    //            Console.WriteLine(errMsg);
+    //        else
+    //            Console.WriteLine(acc0.Balance);
+
+    //    }
+    //}
+}
